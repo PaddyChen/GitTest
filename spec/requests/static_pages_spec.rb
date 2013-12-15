40dc7a61f2ba813053b56ccd92174098335d3576
PaddyChen <lastmemorys@qq.com>
2013-12-15 23:00:45 +0800

    reflactor

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,20 @@
 require 'spec_helper'
 
-describe "Static pages",:type => 'view' do
+describe "Static pages", :type => :feature do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-       # visit '/static_pages/home'
-       # rendered.should have_content('Sample App')
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
     end
   end
+
+
+  describe "About page" do
+    it "should have the content About Page" do
+      visit static_pages_about_path
+      page.should have_selector('nav')
+    end
+  end
+
 end
 
