51a5e3e0bea822887f7f0ee633ef76db57bc1c90
PaddyChen <lastmemorys@qq.com>
2013-12-16 22:20:33 +0800

    last commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
 describe "Static pages", :type => :feature do
+
+  let(:page_title){"SAMPLE APP"}
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector("h1")
     end
   end
 
@@ -12,7 +15,7 @@ describe "Static pages", :type => :feature do
   describe "About page" do
     it "should have the content About Page" do
       visit static_pages_about_path
-      page.should have_selector('nav')
+      page.should have_selector('h1')
     end
   end
 
